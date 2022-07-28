fc9fdbf0139fd2e8460df9afdfc2aa2d30aa8b26
sallaball <sallaball1206@gmail.com>
2022-07-28 19:31:17 +0300

    added more questions and function

--- Develop/index.js
+++ Develop/index.js
@@ -64,7 +64,7 @@ const questions = [
         {
             type: 'confirm',
             name: 'confirmContribtors',
-            message: 'Will you allow others to contribute?'
+            message: 'Will you allow others to contribute?',
             default: true
         },
         {
@@ -130,10 +130,42 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/generated-README.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
 
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+
+    return inquirer.prompt(questions)
+    .then(readmeData => {
+        return readmeData;
+    })
+}
 
 // Function call to initialize app
-init();
+init()
+.then(readmeData => {
+    console.log(readmeData);
+    return generateMarkdown(readmeData);
+})
+.then(pageMD => {
+    return writeFile(pageMD);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse.message);
+})
+.catch(err => {
+    console.log(err);
+})
